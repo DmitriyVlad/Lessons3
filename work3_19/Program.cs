e1dd5f08189aec8c9614d8bdd393a961f1ea165f
Dmitriy <timon@yahoo.com>
2022-12-07 18:09:45 +0300

    Задание 19

---
+++ work3_19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 19
+
+Console.Write("Ваше число:");
+string num = Console.ReadLine();
+int n = 5;
+int itog = 1;
+for (int i = 0; i < n / 2; i++)
+
+    if(num[i] != num[n-i-1])
+       itog = 0;
+    
+if(itog == 0)
+Console.WriteLine("Число " + num + " - обычное");
+else
+Console.WriteLine("Число " + num + " - палиндром");
