5b2344f4a5f420dc3be3139cd8bdce17d6d637a8
Dmitriy <timon@yahoo.com>
2022-12-08 10:05:53 +0300

    Задание 23

---
+++ work3_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 23
+
+int n = new Random().Next(1, 10);
+Console.WriteLine("Число N = " + n);
+Console.WriteLine("Таблица кубов чисел от 1 до " + n + ":");
+for(int i = 1; i < n + 1; i++)
+    {
+        double res = Math.Pow(i, 3);
+        Console.Write(res + "  ");
+    }
+Console.WriteLine("");
